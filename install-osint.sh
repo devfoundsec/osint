6655e9b72512cf1838941f0967cf974231ed41ad
jul10l14 <jul10l1r4@ufrn.edu.br>
2019-06-30 20:38:43 -0300

    :tada: Dependencies install for osint tools

--- install-osint.sh
+++ install-osint.sh
@@ -15,7 +15,7 @@ banner="$(cat ./ascii-art)"
 
 echo "$banner" | while read line
   do
-    printf "$red$line$end\r"
+    printf "$yellow$line$end\r"
     sleep 0.01
     echo -e "\e[38;5;202;1m$line$end"
   done
@@ -31,6 +31,18 @@ _install_git(){
   echo -e "Run: $orange$run$end"
   eval "$run" && echo -e "$correct Installed(s): $1" || echo -e "$incorrect Error in install of: $1" 
 }
+_run(){
+  echo -e "Run: $orange$1$end"
+  eval "$1" && echo -e "$correct $1" || echo -e "$incorrect $1" 
+}
 
-_install_git 'https://github.com/graniet/operative-framework.git'
-_install_pip 'pagodo osrframework'
+# Operative-framework
+_run 'go get github.com/graniet/operative-framework'
+# Install D4N155
+_install_git 'https://github.com/OWASP/D4N155.git'
+_install_pip '-r D4N155/requirements.txt'
+# Install sherlock
+_install_git 'https://github.com/sherlock-project/sherlock.git'
+_install_pip '-r sherlock/requirements.txt'
+# Install osrframework
+_run "$(ls -v /usr/local/bin/pip2* | head -n1 || printf 'pip') install osrframework --user"
